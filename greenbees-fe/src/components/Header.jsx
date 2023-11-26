2f0adf84e7f25802aee67e9c386a5a53e49153c0
typeerror92 <simonwalgenbach@posteo.de>
2023-11-26 02:55:26 +0000

    add background

--- greenbees-fe/src/components/Header.jsx
+++ greenbees-fe/src/components/Header.jsx
@@ -1,15 +1,9 @@
 export function Header (){
     return (
-        <div className="border header">
+        <div className="header">
             <img id='header__logo' src='/logo-500x432.svg'/>
             <h1 id='header__title'>greenbees</h1>
-            <p>
-                2
-            </p>
-            <a className="header__button">About</a>
-            <p>
-                4
-            </p>
+            <a id='header__link'>about</a>
         </div>
     )
 }
--- greenbees-fe/src/components/Main.jsx
+++ greenbees-fe/src/components/Main.jsx
@@ -3,19 +3,7 @@ import Content from "./Content"
 export function Main (){
     return(
         <main className="border main">
-            <div>
-
-            </div>
-            <p className="border">
-                ad_1
-            </p>
             <Content/>
-            <p className="border">
-                ad_2
-            </p>
-            <div>
-
-            </div>
         </main> 
     )
 }
