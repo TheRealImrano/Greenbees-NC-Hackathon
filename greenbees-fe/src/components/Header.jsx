7f6e993ea444c9efa31e443134b0ac881da3f0cd
typeerror92 <simonwalgenbach@posteo.de>
2023-11-26 01:30:07 +0000

    add boundaries to game tiles

--- greenbees-fe/src/components/Header.jsx
+++ greenbees-fe/src/components/Header.jsx
@@ -1,16 +1,12 @@
 export function Header (){
     return (
         <div className="border header">
-            <img height='100%' src='/logo-500x500.svg'/>
-            <div>
-                hi
-            </div>
+            <img id='header__logo' src='/logo-500x432.svg'/>
+            <h1 id='header__title'>greenbees</h1>
             <p>
                 2
             </p>
-            <p>
-                3
-            </p>
+            <a className="header__button">About</a>
             <p>
                 4
             </p>
